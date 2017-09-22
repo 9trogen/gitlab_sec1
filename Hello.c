dbbbf55b7bde82111cf1bd0ac39eb09cfdb38ddf
Affan Dolohmi <affan2409@gmail.com>
2017-09-22 15:41:09 +0700

    next Hello.c

--- Hello.c
+++ Hello.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 void main(){
-printf(" Hello Karen");
-printf("what do you want?");
+	printf(" Hello Karen");
+	printf("what do you want?");
+	printf("What the .. ,why are you too fast?");
 }
